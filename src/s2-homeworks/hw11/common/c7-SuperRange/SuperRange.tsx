fbc99827040188d7137b99cb676599d3d3717263
bakuga <maximgancharik@mail.ru>
2025-06-16 12:43:54 +0300

    обновил проект

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,28 +4,28 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{
-                width: 200,
-                color: 'limegreen',
+            sx={{ // стили для слайдера
+                width: 200, // или другая ширина по дизайну
+                color: 'limegreen', // Основной цвет (активная часть трека и ползунок)
                 '& .MuiSlider-thumb': {
-                    backgroundColor: 'limegreen',
-                    border: '2px solid white',
+                    backgroundColor: 'limegreen', // Цвет ползунка
+                    border: '2px solid white', // Белая обводка ползунка как на дизайне
                     '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-                        boxShadow: 'inherit',
+                        boxShadow: 'inherit', // Убираем стандартную тень при фокусе/наведении
                     },
-                    '&::before': {
+                    '&::before': { // Убираем пульсацию при фокусе, если она есть
                         display: 'none',
                     },
                 },
                 '& .MuiSlider-rail': {
-                    backgroundColor: '#bdbdbd',
+                    backgroundColor: '#bdbdbd', // Цвет неактивной части трека (серый)
                     opacity: 1,
                 },
                 '& .MuiSlider-track': {
-                    border: 'none',
+                    border: 'none', // Убираем границу трека, если она есть и мешает
                 }
             }}
-            {...props}
+            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
 }
