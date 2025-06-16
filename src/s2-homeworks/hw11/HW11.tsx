cfed35d9c2db9d4fc88605fc002f30c7f1d976c1
bakuga <maximgancharik@mail.ru>
2025-06-16 13:33:52 +0300

    22221обновил проект

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -37,7 +37,7 @@ function HW11() {
 
     return (
         <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+            <div className={s2.hwTitle}>Homework #111111111</div>
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
