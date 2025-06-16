482b53adf883df91f9c5a63d659d55583ac249f9
bakuga <maximgancharik@mail.ru>
2025-06-16 12:30:46 +0300

    обновил проект

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react'
-import s from './HW11.module.css'
 import {restoreState, saveState} from '../hw06/localStorage/localStorage'
 import SuperRange from "./common/c7-SuperRange/SuperRange";
+import s from './HW11.module.css'
+import s2 from '../../s1-main/App.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(() => restoreState<number>('hw11-value1', 0))
@@ -28,32 +29,33 @@ function HW11() {
         }
     }
 
-    return (
-        <div id={'hw11'}>
-            <div className={s.hwTitle}>Homework 11</div>
-            <div className={s.hw}>
-                <div className={s.sliderBlock}>
-                    <span id={'hw11-value'} className={s.number}>{value1}</span>
-                    <SuperRange
-                        id={'hw11-single-slider'}
-                        value={value1}
-                        onChange={change}
-                        min={0}
-                        max={99}
-                    />
-                </div>
+    return ( <div id={'hw11'}>
+            <div className={s2.hwTitle}>Homework #11</div>
 
-                <div className={s.sliderBlock}>
-                    <span className={s.number}>{value1}</span>
-                    <SuperRange
-                        id={'hw11-double-slider'}
-                        value={[value1, value2]}
-                        onChange={change}
-                        min={0}
-                        max={99}
-                        disableSwap
-                    />
-                    <span className={s.number}>{value2}</span>
+            <div className={s2.hw}>
+                <div className={s.container}>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-single-slider'}
+                            value={value1}
+                            onChange={(e, val) => change(e, val as number)} // Передаем только число
+                            min={0}
+                            max={99} // Ограничиваем одиночный слайдер
+                        />
+                    </div>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-double-slider'}
+                            value={[value1, value2]}
+                            onChange={change} // Эта функция уже обрабатывает массив
+                            min={0}
+                            max={100}
+                            disableSwap // Важный проп для MUI Slider, чтобы ползунки не менялись местами
+                        />
+                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                    </div>
                 </div>
             </div>
         </div>
