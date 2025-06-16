e4c740ef7b82b14cc5e4c53891907d0d69050ad1
bakuga <maximgancharik@mail.ru>
2025-06-16 12:16:23 +0300

    обновил проект

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,16 +1,105 @@
+
+import React, { useEffect, useState } from 'react'
+import s from './HW11.module.css'
+import s2 from '../../s1-main/App.module.css'
+import { restoreState } from '../hw06/localStorage/localStorage'
+import SuperRange from './common/c7-SuperRange/SuperRange'
+
+function HW11() {
+    const [value1, setValue1] = useState(() => restoreState<number>('hw11-value1', 0))
+    const [value2, setValue2] = useState(() => {
+        const restored = restoreState<number>('hw11-value2', 100)
+        return restored >= 100 ? 99 : restored // ограничение, чтобы второй слайдер не был на краю
+    })
+
+    useEffect(() => {
+        if (value2 >= 100) {
+            setValue2(99)
+            localStorage.setItem('hw11-value2', JSON.stringify(99))
+        }
+    }, [])
+
+    const change = (event: Event, newValue: number | number[]) => {
+        if (Array.isArray(newValue)) {
+            let [newVal1, newVal2] = newValue
+
+            // Защита от равных значений и выхода за 100
+            if (newVal1 === newVal2) newVal1 = newVal2 - 1
+            if (newVal2 > 99) newVal2 = 99
+
+            setValue1(newVal1)
+            setValue2(newVal2)
+        } else {
+            const newSingleValue = newValue as number
+
+            if (newSingleValue >= value2) {
+                setValue1(value2 - 1)
+            } else {
+                setValue1(newSingleValue)
+            }
+        }
+    }
+
+    return (
+        <div id={'hw11'}>
+            <div className={s2.hwTitle}>Homework #11</div>
+
+            <div className={s2.hw}>
+                <div className={s.container}>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-single-slider'}
+                            value={value1}
+                            onChange={(e, val) => change(e, val as number)}
+                            min={0}
+                            max={100}
+                        />
+                    </div>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-double-slider'}
+                            value={[value1, value2]}
+                            onChange={change}
+                            min={0}
+                            max={100}
+                            disableSwap
+                        />
+                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HW11
+
+
+
+
+
+
+
+
+
+
+
+/*
 import React, { useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
-/*
+/!*
 * 1 - передать значения в оба слайдера
 * 2 - дописать типы и логику функции change
 * 3 - сделать стили в соответствии с дизайном
-* */
+* *!/
 
-/*function HW11() {
+/!*function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
@@ -43,13 +132,26 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
                 setValue2(newSingleValue);
             }
         }
-    }*/
+    }*!/
 
 function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0));
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100));
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 99));
 
     const change = (event: Event, newValue: number | number[]) => {
+        if (Array.isArray(newValue)) {
+            let [newVal1, newVal2] = newValue;
+
+            // защита от совпадения
+            if (newVal1 === newVal2) newVal1 = newVal2 - 1;
+
+            // защита от крайних значений
+            if (newVal2 > 99) newVal2 = 99;
+
+            setValue1(newVal1);
+            setValue2(newVal2);
+        }
+
         if (Array.isArray(newValue)) {
             const [newVal1, newVal2] = newValue;
 
@@ -104,4 +206,4 @@ function HW11() {
     )
 }
 
-export default HW11
+export default HW11*/
