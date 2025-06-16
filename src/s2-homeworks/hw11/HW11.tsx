ee0571dbf1f994e0cbd9f24ee324052dbeea14cb
bakuga <maximgancharik@mail.ru>
2025-06-16 12:55:26 +0300

    обновил проект

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -3,44 +3,37 @@ import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-/*
-1 - передать значения в оба слайдера
-2 - дописать типы и логику функции change
-3 - сделать стили в соответствии с дизайном
-*/
+
+
 function HW11() {
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0));
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100));
+
     const change = (event: Event, newValue: number | number[]) => {
         if (Array.isArray(newValue)) {
             // Изменился двойной слайдер
-            const [newVal1, newVal2] = newValue;
-            setValue1(newVal1); // value1 (и для одиночного, и для двойного)
-            setValue2(newVal2); // value2 (только для двойного)
+            // MUI с disableSwap гарантирует, что newValue[0] <= newValue[1]
+            setValue1(newValue[0]);
+            setValue2(newValue[1]);
         } else {
             // Изменился одиночный слайдер
-            // Его значение newValue (это новое значение для value1)
-            // Мы должны убедиться, что value1 не превышает value2,
-            // но и value2 не должен "убегать" от value1, если это не требуется.
+            const newSingleSliderValue = newValue as number;
 
-            // Если одиночный слайдер меняет value1, то левый край двойного слайдера
-            // также должен измениться на это значение.
-            // Правый край (value2) остается неизменным, если newValue (новый value1) не превышает его.
-            // Если newValue (новый value1) становится больше value2,
-            // то value2 должен стать равным newValue, чтобы диапазон оставался валидным
-            // (т.е. левый ползунок не может быть правее правого).
-
-            const newSingleValue = newValue as number;
-            setValue1(newSingleValue);
-
-            // Синхронизация: если одиночный value1 стал больше текущего value2,
-            // то value2 подтягивается к value1.
-            // Это соответствует поведению, где ползунки не пересекаются на двойном слайдере.
-            if (newSingleValue > value2) {
-                setValue2(newSingleValue);
+            // Если новое значение value1 (от одиночного слайдера)
+            // пытается "перепрыгнуть" через текущее value2,
+            // то value2 должен "подтянуться" к этому новому значению.
+            // Оба значения становятся равными.
+            if (newSingleSliderValue > value2) { // value2 здесь - это значение из предыдущего состояния
+                setValue1(newSingleSliderValue);
+                setValue2(newSingleSliderValue);
+            } else {
+                // В противном случае (newSingleSliderValue <= value2),
+                // меняется только value1. value2 остается на месте.
+                setValue1(newSingleSliderValue);
+                // setValue2(value2); // Нет необходимости вызывать setValue2, если оно не меняется
             }
         }
-    }
+    };
 
     return (
         <div id={'hw11'}>
@@ -53,26 +46,27 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             value={value1}
-                            onChange={(e, val) => change(e, val as number)} // Передаем только число
+                            onChange={(e, val) => change(e, val as number)}
                             min={0}
-                            max={100} // Ограничиваем одиночный слайдер
+                            max={100}
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
-                            value={[value1, value2]}
-                            onChange={change} // Эта функция уже обрабатывает массив
+                            value={[value1, value2]} // Всегда передаем массив [value1, value2]
+                            onChange={change}
                             min={0}
                             max={100}
-                            disableSwap // Важный проп для MUI Slider, чтобы ползунки не менялись местами
+                            disableSwap // Важный проп для MUI Slider
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-export default HW11
+
+export default HW11;
